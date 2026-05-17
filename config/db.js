6b621b52d0edbf1fd17038de245191425b2007ef
Irtiaj <irtiaj0541@gmail.com>
2026-05-17 23:15:45 +0600

    Seted up Database connection pool and environment variables

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const {pool} = require('pg')
+
+require('dotenv').config()
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+});
+
+pool.connect((err,client,release) => {
+    if(err){
+        return console.error('error while aquiring the client ',err.stack);
+    }
+
+    console.log('Logged In');
+    release()
+});
+
+module.exports = pool;
