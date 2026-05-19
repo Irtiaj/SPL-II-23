4ae118074de2b2a09d59aad91c4204724a71b3c5
Irtiaj <irtiaj0541@gmail.com>
2026-05-19 16:31:02 +0600

    Added the Post() part for submitting the new report

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -20,4 +20,28 @@ router.get('/', async(req,res) =>{
     }
 });
 
+router.post('/', async(req,res) => {
+    try{
+        const{ description, longitude, latitude, ward_id, before_photo_url, after_photo_url } = req.body;
+
+        const newReport = await pool.query(`insert into reports (description, location, ward_id, before_photo_url, after_photo_url, status, created_at, updated_at)
+            values ($1,ST_SetSRID(ST_MAKEPOINT($2,$3), 4326), $4, $5, $6, 'Pending', NOW(), NOW()) RETURNING *`, 
+        [description, longitude, latitude, ward_id, before_photo_url, after_photo_url || null]) ;
+
+        res.status(201).json({
+            success: true,
+            message: "Report created successfully",
+            data: newReport.rows[0]
+        })
+    }
+    catch(error){
+        console.error("Error occured while saving the report ",error.message)
+
+        res.status(500).json({
+            success: false,
+            message: "Couldn't save"
+        });
+    }
+});
+
 module.exports = router
