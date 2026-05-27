dd9ef9ed51669dfe2e61e01bc6d0969d41f5b02e
Irtiaj <irtiaj0541@gmail.com>
2026-05-27 21:39:58 +0600

    added the login part

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -33,4 +33,56 @@ router.post('/register', async(req,res) => {
     }
 })
 
+router.post('/login', async(req,res) => {
+    try{
+        const {email, phone_number, password} = req.body
+
+        const uniqe_identifier = email || phone_number
+
+        const userInput = await pool.query('SELECT * FROM users WHERE email = $1 OR phone_number = $1', [uniqe_identifier])
+
+        if(userInput.rows.length === 0){
+            return res.status(401).json({
+                success: false,
+                message: "Invalid userId or Password"
+            })
+        }
+
+        const user = userInput.rows[0]
+
+        const matchedPass = await bcrypt.compare(password, user.password_hash)
+
+        if(!matchedPass){
+            return res.status(401).json({
+                success: false,
+                message: "Invalid userId or Password"
+            })
+        }
+
+        const tokeninfo = {
+            user_id: user.user_id,
+            role: user.role
+        }
+
+        const token = jsonwebtoken.sign(tokeninfo,process.env.Jeson_Web_Token, {expiresIn: '1d'})
+
+        res.status(200).json({
+            success: true,
+            message: "Successfully Logged In",
+            token: token,
+            user: {
+                user_id: user.user_id,
+                full_name: user.full_name,
+                role: user.role
+            }
+        })
+    }
+    catch(error){
+        res.status(500).json({
+            success: false,
+            message: "Failed to log in"
+        })
+    }
+})
+
 module.exports = router;
