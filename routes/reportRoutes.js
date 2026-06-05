9d1e06d9304eadaf1f33891d5d54ff40d00fbdb0
Irtiaj <irtiaj0541@gmail.com>
2026-06-05 23:39:04 +0600

    Modified for the PostGIS

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -24,15 +24,16 @@ router.get('/', async(req,res) =>{
 
 router.post('/', async(req,res) => {
     try{
-        const{ description, longitude, latitude, ward_id, before_photo_url, after_photo_url } = req.body;
+        const{ description, longitude, latitude, before_photo_url, after_photo_url } = req.body;
 
         const newReport = await pool.query(`insert into reports (description, location, ward_id, before_photo_url, after_photo_url, status, created_at, updated_at)
-            values ($1,ST_SetSRID(ST_MAKEPOINT($2,$3), 4326), $4, $5, $6, 'Pending', NOW(), NOW()) RETURNING *`, 
-        [description, longitude, latitude, ward_id, before_photo_url, after_photo_url || null]) ;
+            values ($1,ST_SetSRID(ST_MAKEPOINT($2,$3), 4326), (SELECT ward_id fROM wards WHERE ST_Contains(boundaries,ST_SetSRID(ST_MakePoint($2, $3), 4326)))
+            ,$4, $5,'Pending', NOW(), NOW()) RETURNING *`, 
+        [description, longitude, latitude, before_photo_url, after_photo_url || null]) ;
 
         res.status(201).json({
             success: true,
-            message: "Report created successfully",
+            message: "Report created successfully & passed to the respected Councillor",
             data: newReport.rows[0]
         })
     }
